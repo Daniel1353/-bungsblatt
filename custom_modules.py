251453c4e8eb590a4f41fd3d45b735ff2deb5194
Daniel Pleyer <Irgend@mail.com>
2024-04-04 10:22:33 +0200

    Absichtliche fehler eingebaut

--- custom_modules.py
+++ custom_modules.py
@@ -1,4 +1,4 @@
 def say_hi(name):
-    print(f"Herzlich Wilkommen {name} und einen schönen Tag wünscht dir dein Betriebsystem")
+    print(f"Herzlich Wilkommen {name} und einen schönen Tag wünscht dir dein Betriebsystem");
 
-say_hi(input("Wie lautet dein Name: "))
+say_hi(input("Wie lautet dein Name: "));
