dcd70214315b4b2ce14886e4c5774941346fec21
Daniel Pleyer <Irgend@mail.com>
2024-04-04 10:16:19 +0200

    Wilkommensfunktion erstellt, in das Hauptprogramm eingefügt und erfolgreich getestet

---
+++ custom_modules.py
@@ -0,0 +1,4 @@
+def say_hi(name):
+    print(f"Herzlich Wilkommen {name} und einen schönen Tag wünscht dir dein Betriebsystem")
+
+say_hi(input("Wie lautet dein Name: "))
--- programm.py
+++ programm.py
@@ -1,3 +1,6 @@
+import custom_modules
+
+custom_modules.say_hi(input("Wie lautet dein Name: "))
 
 for i in range(3):
     print("Der Gärtner ist ein böser Mann")
